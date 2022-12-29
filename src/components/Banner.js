dff76f8f19ae8c77a7f36ec12c5127ec20187895
SashaWouse <snowpill@hotmail.fi>
2022-12-29 17:54:40 +0200

    Header banner CSS

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,46 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header.png";
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
     const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const [text, setText] = useState('');
+    const [index, setIndex] = useState(1);
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
 
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
 
     return (
         <section className="banner" id="home">
@@ -13,7 +51,7 @@ export const Banner = () => {
                 <Row className="aligh-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to My Portfolio</span>
-                        <h1>{`Hi I'm Alexandra`}<span className="wrap">web developer</span></h1>
+                        <h1>{`Hi I'm Alexandra `}<br></br><span className="wrap">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                     </Col>
