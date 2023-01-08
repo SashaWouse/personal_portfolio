b6ef4cb45050fd46f674a7a0fba99822fab6e27b
SashaWouse <snowpill@hotmail.fi>
2023-01-09 00:04:46 +0200

    server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cors = require('cors');
-const nodemailer = require('nodemailer');
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
-// server used to send emails
+// server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,43 +12,41 @@ app.listen(5000, () => console.log("Server Running"));
 // console.log(process.env.EMAIL_USER);
 // console.log(process.env.EMAIL_PASS);
 
-const contactEmail = modemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "********@gmail.com",
-        pass: ""
-    },
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "********@gmail.com",
+    pass: ""
+  },
 });
 
 contactEmail.verify((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("ready to Send")
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
 });
 
 router.post("/contact", (req, res) => {
-    const name = req.body.firstName + req.body.lastName;
-    const email = req.body.email;
-    const message = req.body.message;
-    const phone = req.body.phone;
-    const mail = {
-        from: name,
-        to: "********@gmail.com",
-        subject: "Contact From Submission - Portfolio",
-        html: `<p>Name: ${name}</p>
-                <p>Email: ${email}</p>
-                <p>Phone: ${phone}</p>
-                <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-        if (error) {
-            res.json(error);
-        } else {
-            res.json({code: 200, status: "Message Sent"});
-        }
-    });
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "********@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
 });
-
-
